4332570fdb9ec580bf2e177375432fc153cf5889
Fabian <f.j.p.ballast@student.tudelft.nl>
2021-02-06 13:17:09 +0100

    Simple test program

--- MSD/src/main.cpp
+++ MSD/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
+#define DIR1 10
+#define DIR2 11
+#define PWM 9
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(DIR1, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+  pinMode(PWM, OUTPUT);
+
+  digitalWrite(DIR1, HIGH);
+  digitalWrite(DIR2, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+ analogWrite(PWM, 100); 
 }
